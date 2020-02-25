670c3eff6ebb926573a27638af9ed5ce881e1c39
Gabe Nydick <martialartsapplemma@gmail.com>
2020-02-24 23:24:43 -0500

    making repo active for testing

--- push.sh
+++ push.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-dpkg-scanpackages ./debs > Packages
-bzip2 -fks Packages
+#dpkg-scanpackages ./debs > Packages
+#bzip2 -fks Packages
+find ./debs -type f -name '*.deb' -delete
+find . -name '.DS_Store' -type f -delete
+rm -r packages.bz2
