bc3eed54d9a4dd5e35020b96d87f65b3e2173140
Gabe Nydick <martialartsapplemma@gmail.com>
2020-02-24 23:49:15 -0500

    changes

--- packages.sh
+++ packages.sh
@@ -5,7 +5,6 @@ dpkg-deb -bZgzip projects/CydiaLogoTheme debs
 dpkg-deb -bZgzip projects/SimplifiedRespringLogo debs
 dpkg-deb -bZgzip projects/CydiaIcons debs
 dpkg-deb -bZgzip projects/Simplified debs
-dpkg-scanpackages -bZgzip projects/SimplifiedRespringLogo debs
 dpkg-deb -bZgzip projects/iOS11BetaProfile debs
 dpkg-deb -bZgzip projects/MontyHacks_Theme debs
 dpkg-deb -bZgzip projects/ShapeYourIcons debs
